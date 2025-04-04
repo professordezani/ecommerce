8a448369c9828818567dd576097c6bcee04394ab
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-04-04 14:42:51 -0300

    Adiciona Views

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -5,19 +5,30 @@ namespace Ecommerce.Controllers;
 
 public class ProdutoController : Controller
 {
-    public ActionResult Index()
+    // atributos:
+    List<Produto> produtos = new List<Produto>();
+
+    public ProdutoController()
     {
-        // No futuro, conectaremos com o BD
-        List<Produto> produtos = new List<Produto>();
         produtos.Add(new Produto { ProdutoId = 1, Nome = "Manga", Imagem="manga.jpg", Preco = 10 });
         produtos.Add(new Produto { ProdutoId = 2, Nome = "Banana", Imagem="banana.jpg", Preco = 5});
         produtos.Add(new Produto { ProdutoId = 3, Nome = "Goiaba", Imagem="goiaba.jpg", Preco = 18 });
+    }
 
+    public ActionResult Index()
+    {
         return View(produtos); // Views/Produto/Index.cshtml
     }
 
     public ActionResult Detalhes(int id)
     {
-        return View();
+        foreach(var produto in produtos)
+        {
+            if(produto.ProdutoId == id) {
+                return View(produto);
+            }
+        }
+
+        return NotFound();
     }
 }
